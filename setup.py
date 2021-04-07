a72a267198893030a17f4f5de7199a1efd0c08f8
semantic-release <semantic-release>
2021-04-07 10:58:16 +0200

    bump: 1.3.8
    Automatically generated by python-semantic-release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if os.path.isdir("build"):
 from distutils.core import setup
 from setuptools.command.install import install
 
-__version__ = '1.3.7'
+__version__ = '1.3.8'
 
 def download_files():
     try:
