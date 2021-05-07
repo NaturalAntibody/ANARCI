aad01cc45829f9c4116e9be9f9e05e6b0b629948
Piotr Deszyński <deszynski@red-sky.pl>
2021-05-07 15:22:55 +0200

    feat(build): Detect platform and link correct muscle bin

--- setup.py
+++ setup.py
@@ -5,13 +5,19 @@
 # Maintained by members of OPIG #
 #                               #
 
-import shutil, os, subprocess, importlib, sys
+import importlib
+import os
+import shutil
+import subprocess
+import sys
+from pathlib import Path
 
 # Clean this out if it exists
 if os.path.isdir("build"):
     shutil.rmtree("build/")
 
 from distutils.core import setup
+
 from setuptools.command.install import install
 
 __version__ = '1.3.9'
@@ -24,33 +30,41 @@ def download_files():
         sys.stderr.write(str(e))
         sys.exit(1)
 
-    os.chdir("build_pipeline")
-
+    # os.chdir("build_pipeline")
+    build_path = Path('build_pipeline')
     try:
         # shutil.rmtree("curated_alignments/")
         # shutil.rmtree("muscle_alignments/")
-        shutil.rmtree("HMMs/")
+        shutil.rmtree(build_path / "HMMs/")
         # shutil.rmtree("IMGT_sequence_files/")
         os.mkdir(os.path.join(ANARCI_LOC, "dat"))
     except OSError:
         pass
 
     print('Downloading germlines from IMGT and building HMMs...')
-    proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE, cwd="build_pipeline")
     o, e = proc.communicate()
 
     print(o.decode())
     print(e.decode())
 
-    shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
-    shutil.rmtree(os.path.join(ANARCI_LOC, "dat/HMMs/"))
-    shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
+    shutil.copy( build_path / "curated_alignments/germlines.py", ANARCI_LOC )
+    hmms_path = os.path.join(ANARCI_LOC, "dat/HMMs/")
+    if os.path.exists(hmms_path):
+        shutil.rmtree(hmms_path)
+    shutil.copytree( build_path / "HMMs", hmms_path )
 
-
+def link_muscle(scripts_path: str) -> None:
+    muscle_file = 'muscle_macOS' if sys.platform == 'darwin' else 'muscle_linux'
+    os.symlink(os.path.join(scripts_path, muscle_file), os.path.join(scripts_path, 'muscle'))
+    
 class Install(install):
 
     def run(self):
         super().run()
+        import pprint
+        pprint.pprint(vars(self))
+        link_muscle(self.install_scripts)
         download_files()
 
 
@@ -69,7 +83,7 @@ setup(name='anarci',
                               'dat/HMMs/ALL.hmm.h3p']},
       scripts=['bin/ANARCI'],
       install_requires=['biopython>=1.78'],
-      data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS']) ],
+      data_files = [ ('bin', ['bin/muscle_linux', 'bin/muscle_macOS']) ],
       cmdclass={'install': Install}
      )
 
